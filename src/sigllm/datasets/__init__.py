4ca388cf0eea5e9402ac02bc8501e84c1550f5eb
Bùi Nguyên Quốc Bảo <quocbao10122001@gmail.com>
2026-02-19 19:17:40 +0700

    feat: include process_warm_cold in __all__ for dataset preprocessing module

--- src/sigllm/datasets/__init__.py
+++ src/sigllm/datasets/__init__.py
@@ -1,5 +1,6 @@
 """Dataset preprocessing for SigLLM."""
 
 from .data_preprocessing import build_ml1m
+from .preprocess_test_cold_warm import process_warm_cold
 
-__all__ = ["build_ml1m"]
+__all__ = ["build_ml1m", "process_warm_cold"]
