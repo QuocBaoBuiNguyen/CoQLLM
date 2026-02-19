2f3a0b9c87d1428752475d7650217832055629ca
Bùi Nguyên Quốc Bảo <quocbao10122001@gmail.com>
2026-02-19 19:49:25 +0700

    feat: update save_file path in train_baseline_model and ensure directory exists

--- src/sigllm/pipelines/rec/train_rec_baseline.py
+++ src/sigllm/pipelines/rec/train_rec_baseline.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import time as time
 import random
@@ -225,5 +226,6 @@ if __name__ == "__main__":
         "patience":50,
         "batch_size":1024
     }
-    save_file = "/content/mf/mf_model.pth"
+    save_file = "/content/SigLLM/mf/mf_model.pth"
+    os.makedirs(os.path.dirname(save_file), exist_ok=True)
     train_baseline_model(train_config, save_mode=True, save_file=save_file, need_train=True, warm_or_cold='warm')
