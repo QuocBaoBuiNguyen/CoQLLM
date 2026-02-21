3dde7e141d69b8f3e348fae15619b079fc71b268
Bùi Nguyên Quốc Bảo <quocbao10122001@gmail.com>
2026-02-21 21:31:22 +0700

    feat: expose QFormerAlignmentDataset in dataset module

--- src/sigllm/datasets/__init__.py
+++ src/sigllm/datasets/__init__.py
@@ -2,5 +2,6 @@
 
 from .data_preprocessing import build_ml1m
 from .preprocess_test_cold_warm import process_warm_cold
+from .qformer.qformer_alignment_dataset import QFormerAlignmentDataset
 
-__all__ = ["build_ml1m", "process_warm_cold"]
+__all__ = ["build_ml1m", "process_warm_cold", "QFormerAlignmentDataset"]
