4a6a895f3b27370ffd6bcfb9a53c0d6bf5e3e186
Bùi Nguyên Quốc Bảo <quocbao10122001@gmail.com>
2026-03-10 17:12:12 +0700

    feat: add logging for configuration parameters in pretty_print method

--- src/sigllm/common/config.py
+++ src/sigllm/common/config.py
@@ -1,3 +1,5 @@
+import logging
+
 from omegaconf import OmegaConf
 from sigllm.common import registry
 
@@ -52,3 +54,22 @@ class Config:
     @property
     def model_cfg(self):
         return self.config.model
+
+    
+    def pretty_print(self):
+        logging.info("\n=====  Running Parameters    =====")
+        logging.info(self._convert_node_to_json(self.config.run))
+
+        logging.info("\n======  Dataset Attributes  ======")
+        datasets = self.config.datasets
+
+        for dataset in datasets:
+            if dataset in self.config.datasets:
+                logging.info(f"\n======== {dataset} =======")
+                dataset_config = self.config.datasets[dataset]
+                logging.info(self._convert_node_to_json(dataset_config))
+            else:
+                logging.warning(f"No dataset named '{dataset}' in config. Skipping")
+
+        logging.info(f"\n======  Model Attributes  ======")
+        logging.info(self._convert_node_to_json(self.config.model))
