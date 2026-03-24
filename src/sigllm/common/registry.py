17f9fc07cec694762cae95c5205440429bd33985
Bùi Nguyên Quốc Bảo <quocbao10122001@gmail.com>
2026-03-24 18:30:54 +0700

    feat: implement register method in Registry class for dynamic item registration

--- src/sigllm/common/registry.py
+++ src/sigllm/common/registry.py
@@ -39,6 +39,30 @@ class Registry:
     def get_builder_class(cls, name):
         return cls.mapping["builder_name_mapping"].get(name, None)
 
+    
+    @classmethod
+    def register(cls, name, obj):
+        r"""Register an item to registry with key 'name'
+
+        Args:
+            name: Key with which the item will be registered.
+
+        Usage::
+
+            from minigpt4.common.registry import registry
+
+            registry.register("config", {})
+        """
+        path = name.split(".")
+        current = cls.mapping["state"]
+
+        for part in path[:-1]:
+            if part not in current:
+                current[part] = {}
+            current = current[part]
+
+        current[path[-1]] = obj
+
     @classmethod
     def register_task(cls, name):
         r"""Register a task to registry under a given name.
